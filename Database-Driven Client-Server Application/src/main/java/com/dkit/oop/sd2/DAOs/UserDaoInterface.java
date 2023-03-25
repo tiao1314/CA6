b17d6afcaf51d4f99caa290449138eef15a62d81
tiao1314 <1314tiao@gmail.com>
2023-03-25 21:15:49 +0000

    UserDaoInterface findUserById

--- Database-Driven Client-Server Application/src/main/java/com/dkit/oop/sd2/DAOs/UserDaoInterface.java
+++ Database-Driven Client-Server Application/src/main/java/com/dkit/oop/sd2/DAOs/UserDaoInterface.java
@@ -11,7 +11,7 @@ public interface UserDaoInterface {
     // Feature 1 – Find all Entities e.g. findAllPlayers() to return a List of all the entities and display that list
     List<User> findAllUsers() throws DaoException;
 
-    
-    
+    //Feature 2 – Find and Display (a single) Entity by Key e.g. findPlayerById( id ) – return a single entity and display its contents.
+    public User findUserById(int id) throws DaoException;
 }
 
